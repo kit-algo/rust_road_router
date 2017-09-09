e8c8df0f09525a4e6f54f1705ab1cb886193d40d
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-09-09 09:35:28 +0200

    use random nodes, visualize search space

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 extern crate bmw_routing_engine;
 extern crate svg;
+extern crate rand;
 
 use std::process::Command;
 use std::env;
 use std::path::Path;
+use std::cmp::max;
 
 use bmw_routing_engine::*;
 use graph::first_out_graph::FirstOutGraph as Graph;
 use graph::*;
 use shortest_path::DijkstrableGraph;
+use shortest_path::query::dijkstra::Server as DijkServer;
 use io::read_into_vector;
 
 use svg::Document;
@@ -54,31 +57,62 @@ fn main() {
     let mut document = Document::new();
     let graph = Graph::new(first_out, head, travel_time);
 
-    let min_lat = 48.992758;
-    let max_lat = 49.043633;
-    let min_lon = 8.340608;
-    let max_lon = 8.489897;
+    let s = rand::random::<NodeId>() % (graph.num_nodes() as NodeId);
+    let t = rand::random::<NodeId>() % (graph.num_nodes() as NodeId);
+    let mut query_server = DijkServer::new(graph.clone());
+    query_server.distance(s, t);
+
+    let mut min_lat = lat.iter().enumerate().filter(|&(i, _)| query_server.is_in_searchspace(i as NodeId) ).map(|(_, &num)| num).min_by_key(|&num| NonNan::new(num)).unwrap();
+    let mut max_lat = lat.iter().enumerate().filter(|&(i, _)| query_server.is_in_searchspace(i as NodeId) ).map(|(_, &num)| num).max_by_key(|&num| NonNan::new(num)).unwrap();
+    let mut min_lon = lon.iter().enumerate().filter(|&(i, _)| query_server.is_in_searchspace(i as NodeId) ).map(|(_, &num)| num).min_by_key(|&num| NonNan::new(num)).unwrap();
+    let mut max_lon = lon.iter().enumerate().filter(|&(i, _)| query_server.is_in_searchspace(i as NodeId) ).map(|(_, &num)| num).max_by_key(|&num| NonNan::new(num)).unwrap();
+
+    let delta_lat = max_lat - min_lat;
+    min_lat -= max(NonNan::new(delta_lat * 0.1), NonNan::new(0.001)).unwrap().0;
+    max_lat += max(NonNan::new(delta_lat * 0.1), NonNan::new(0.001)).unwrap().0;
+    let delta_lon = max_lon - min_lon;
+    min_lon -= max(NonNan::new(delta_lon * 0.1), NonNan::new(0.001)).unwrap().0;
+    max_lon += max(NonNan::new(delta_lon * 0.1), NonNan::new(0.001)).unwrap().0;
 
     for node in 0..graph.num_nodes() {
+        let node_in_searchspace = query_server.is_in_searchspace(node as NodeId);
+
         for Link { node: neighbor, .. } in graph.neighbor_iter(node as NodeId) {
             if (lat[node as usize] <= max_lat && lat[node as usize] >= min_lat && lon[node as usize] <= max_lon && lon[node as usize] >= min_lon)
                 || (lat[neighbor as usize] <= max_lat && lat[neighbor as usize] >= min_lat && lon[neighbor as usize] <= max_lon && lon[neighbor as usize] >= min_lon) {
-                let line = Line::new()
+                let mut line = Line::new()
                     .set("x1", format!("{}", lon[node as usize]))
                     .set("y1", format!("{}", lat[node as usize]))
                     .set("x2", format!("{}", lon[neighbor as usize]))
                     .set("y2", format!("{}", lat[neighbor as usize]))
                     .set("class", "edge");
 
+                if node_in_searchspace && query_server.is_in_searchspace(neighbor) {
+                    line = line.set("class", "edge searchspace");
+                }
+
                 document = document.add(line);
             }
-
         }
     }
-    // let &min_lat = lat.iter().min_by_key(|&&num| NonNan::new(num)).unwrap();
-    // let &max_lat = lat.iter().max_by_key(|&&num| NonNan::new(num)).unwrap();
-    // let &min_lon = lon.iter().min_by_key(|&&num| NonNan::new(num)).unwrap();
-    // let &max_lon = lon.iter().max_by_key(|&&num| NonNan::new(num)).unwrap();
+
+    let mut prev = None;
+    for &node in query_server.path().iter() {
+        match prev {
+            Some(prev_node) => {
+                let line = Line::new()
+                    .set("x1", format!("{}", lon[node as usize]))
+                    .set("y1", format!("{}", lat[node as usize]))
+                    .set("x2", format!("{}", lon[prev_node as usize]))
+                    .set("y2", format!("{}", lat[prev_node as usize]))
+                    .set("class", "edge searchspace shortest_path");
+
+                document = document.add(line);
+            },
+            None => (),
+        }
+        prev = Some(node);
+    }
 
     document = document
         .set("viewBox", (min_lon, min_lat, max_lon - min_lon, max_lat - min_lat))
