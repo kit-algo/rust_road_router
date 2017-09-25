a997f16c08fff62cf0e5182b29fd3d94f325df1f
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-09-25 16:28:35 +0200

    fall back to nodes if no link geometry is present

--- src/import/here/mod.rs
+++ src/import/here/mod.rs
@@ -76,12 +76,21 @@ impl RdfNavLink {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct RdfNode {
     node_id: i64,
     lat: i64,
     lon: i64,
     z_coord: Option<i64>
+}
+
+impl RdfNode {
+    fn as_wgs84(&self) -> WGS84<f64> {
+        WGS84::new(
+            (self.lat as f64) / 100000.,
+            (self.lon as f64) / 100000.,
+            (self.z_coord.unwrap_or(0) as f64) / 100.)
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -187,6 +196,14 @@ pub fn read_graph(source: &RdfDataSource) -> FirstOutGraph {
         geometries.sort_by_key(|geometry| geometry.seq_num);
     }
 
+    let mut nodes: Vec<RdfNode> = vec![RdfNode { node_id: 0, lat: 0, lon: 0, z_coord: None }; n];
+    for node in source.nodes().into_iter() {
+        match node_id_mapping.get(node.node_id as usize) {
+            Some(index) => nodes[index] = node,
+            None => (),
+        }
+    }
+
     // init other graph arrays
     let mut head: Vec<NodeId> = vec![0; m as usize];
     let mut weights: Vec<Weight> = vec![0; m as usize];
@@ -197,7 +214,14 @@ pub fn read_graph(source: &RdfDataSource) -> FirstOutGraph {
         match link_indexes.get(link.link_id as usize) {
             Some(link_index) => {
                 let nav_link = &nav_links[link_index];
-                let weight = (calculate_length_in_m(&link_geometries[link_index]) / nav_link.speed_in_m_per_s()).round() as Weight;
+                let length = if link_geometries[link_index].is_empty() {
+                    let head = &nodes[node_id_mapping.at(link.ref_node_id as usize)];
+                    let tail = &nodes[node_id_mapping.at(link.nonref_node_id as usize)];
+                    head.as_wgs84().distance(&tail.as_wgs84())
+                } else {
+                    calculate_length_in_m(&link_geometries[link_index])
+                };
+                let weight = (length / nav_link.speed_in_m_per_s()).round() as Weight;
 
                 match nav_link.travel_direction {
                     RdfLinkDirection::FromRef => {
