78a20844380d5b03645b38fb5985a0b468228aa4
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-10-30 10:41:13 +0100

    implement skeleton of routing query handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 #![feature(plugin)]
+#![feature(custom_derive)]
 #![plugin(rocket_codegen)]
 
+#[macro_use]
+extern crate serde_derive;
+
 extern crate rocket;
+extern crate rocket_contrib;
 use std::path::{Path, PathBuf};
 use rocket::response::NamedFile;
+use rocket_contrib::Json;
+
+#[derive(Debug, FromForm)]
+struct RoutingQuery {
+    from_lat: f32,
+    from_lng: f32,
+    to_lat: f32,
+    to_lng: f32
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RoutingQueryResponse {
+    // TODO Weight
+    distance: u32,
+    path: Vec<(f32, f32)>
+}
 
 #[get("/")]
-fn hello() -> Option<NamedFile> {
+fn index() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/index.html")).ok()
 }
 
@@ -15,12 +36,18 @@ fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
+#[get("/query?<query_params>", format = "application/json")]
+fn query(query_params: RoutingQuery) -> Json<Option<RoutingQueryResponse>> {
+    let RoutingQuery { from_lat, from_lng, to_lat, to_lng } = query_params;
+    Json(Some(RoutingQueryResponse { distance: 0, path: vec![(from_lat, from_lng), (to_lat, to_lng)] }))
+}
+
 fn main() {
     let config = rocket::config::Config::build(rocket::config::Environment::Staging)
         .port(8888)
         .finalize().expect("Could not create config");
 
     rocket::custom(config, false)
-        .mount("/", routes![hello, files])
+        .mount("/", routes![index, files, query])
         .launch();
 }
