732d75161fe87ee28ef7d1b2964935f6260bab1b
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2017-12-12 14:54:31 +0100

    add another test

--- engine/src/bin/link_speed_estimates.rs
+++ engine/src/bin/link_speed_estimates.rs
@@ -411,4 +411,24 @@ mod tests {
                 LinkSpeedData { link_id: 3, link_entered_timestamp: 101750, estimate_quality: 0.0625, velocity: 72.0 },
             ]);
     }
+
+    #[test]
+    fn three_points_three_links() {
+        let links = vec![
+            LinkData { link_id: 1, length: 10000, speed_limit: 80 },
+            LinkData { link_id: 2, length: 30000, speed_limit: 80 },
+            LinkData { link_id: 3, length: 10000, speed_limit: 80 },
+        ];
+        let traces = vec![
+            TraceData { timestamp: 100000, link_id: 1, traversed_in_travel_direction_fraction: 0.5 },
+            TraceData { timestamp: 100250, link_id: 1, traversed_in_travel_direction_fraction: 1.0 },
+            TraceData { timestamp: 102000, link_id: 3, traversed_in_travel_direction_fraction: 0.5 }
+        ];
+        assert_eq!(calculate(Box::new(links.iter()), Box::new(traces.iter())).unwrap(),
+            vec![
+                LinkSpeedData { link_id: 1, link_entered_timestamp: 99750, estimate_quality: 0.5, velocity: 72.0 },
+                LinkSpeedData { link_id: 2, link_entered_timestamp: 100250, estimate_quality: 30000.0 / 35000.0, velocity: 72.0 },
+                LinkSpeedData { link_id: 3, link_entered_timestamp: 101750, estimate_quality: 0.5 * 5000.0 / 35000.0, velocity: 72.0 },
+            ]);
+    }
 }
