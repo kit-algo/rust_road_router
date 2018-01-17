c777efe56b7cd714a529066cf7fec4f872344e53
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-01-17 11:07:43 +0100

    removing now unnecessary dijkstrable graph trait

--- engine/src/shortest_path/mod.rs
+++ engine/src/shortest_path/mod.rs
@@ -11,13 +11,3 @@ pub struct Query {
     from: NodeId,
     to: NodeId
 }
-
-pub trait DijkstrableGraph {
-    fn num_nodes(&self) -> usize;
-    // not particularily liking this interface, would be much nicer to return an iterator
-    // sadly we would have to box it, which would be problematic in terms of performance
-    // even the impl trait functionality on nightly won't allow generic return types on traits
-    // which makes sense when you think about it, because it would need to return something
-    // which does dynamic dispath, which is exactly what the boxing would do...
-    fn for_each_neighbor(&self, node: NodeId, f: &mut FnMut(Link));
-}
