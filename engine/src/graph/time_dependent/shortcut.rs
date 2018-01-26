fa2bc43a1a9fbbdf680cd8c81a238a155a7041e0
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-01-26 16:47:20 +0100

    make merge immutable and return new shortcut

--- engine/src/graph/time_dependent/shortcut.rs
+++ engine/src/graph/time_dependent/shortcut.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Shortcut {
     source_data: Vec<ShortcutData>,
     time_data: Vec<Timestamp>
@@ -18,100 +18,94 @@ impl Shortcut {
         Shortcut { source_data, time_data }
     }
 
-    pub fn merge(&mut self, other: Linked, original_graph: &TDGraph, shortcut_graph: &ShortcutGraph) {
+    pub fn merge(&self, other: Linked, original_graph: &TDGraph, shortcut_graph: &ShortcutGraph) -> Shortcut {
         if self.source_data.is_empty() {
-            self.source_data.push(other.as_shortcut_data());
-            self.time_data.push(0);
-        } else {
-            // TODO bounds for range
-            let (current_lower_bound, current_upper_bound) = self.bounds(original_graph, shortcut_graph);
-            let (other_lower_bound, other_upper_bound) = other.bounds(original_graph, shortcut_graph);
-            if current_lower_bound >= other_upper_bound {
-                self.source_data.clear();
-                self.time_data.clear();
-                self.source_data.push(other.as_shortcut_data());
-                self.time_data.push(0);
-                return
-            } else if other_lower_bound >= current_upper_bound {
-                return
-            }
+            return Shortcut { source_data: vec![other.as_shortcut_data()], time_data: vec![0] }
+        }
 
-            let current_initial_value = self.evaluate(0, original_graph, shortcut_graph);
-            let other_initial_value = other.evaluate(0, original_graph, shortcut_graph);
+        // TODO bounds for range
+        let (current_lower_bound, current_upper_bound) = self.bounds(original_graph, shortcut_graph);
+        let (other_lower_bound, other_upper_bound) = other.bounds(original_graph, shortcut_graph);
+        if current_lower_bound >= other_upper_bound {
+            return Shortcut { source_data: vec![other.as_shortcut_data()], time_data: vec![0] }
+        } else if other_lower_bound >= current_upper_bound {
+            return self.clone()
+        }
 
-            let is_current_initially_lower = current_initial_value <= other_initial_value;
-            let mut is_current_lower_for_prev_ipp = is_current_initially_lower;
-            let mut self_next_ipp = self.next_ipp_greater_eq(0, original_graph, shortcut_graph);
-            let mut other_next_ipp = other.next_ipp_greater_eq(0, original_graph, shortcut_graph);
-            let mut better_way = vec![];
+        let current_initial_value = self.evaluate(0, original_graph, shortcut_graph);
+        let other_initial_value = other.evaluate(0, original_graph, shortcut_graph);
 
-            while self_next_ipp.is_some() || other_next_ipp.is_some() {
-                let ipp = match (self_next_ipp, other_next_ipp) {
-                    (Some(self_next_ipp_at), Some(other_next_ipp_at)) => {
-                        if self_next_ipp_at <= other_next_ipp_at {
-                            self_next_ipp = self.next_ipp_greater_eq(self_next_ipp_at + 1, original_graph, shortcut_graph);
-                            self_next_ipp_at
-                        } else {
-                            other_next_ipp = other.next_ipp_greater_eq(other_next_ipp_at + 1, original_graph, shortcut_graph);
-                            other_next_ipp_at
-                        }
-                    },
-                    (None, Some(other_next_ipp_at)) => {
-                        other_next_ipp = other.next_ipp_greater_eq(other_next_ipp_at + 1, original_graph, shortcut_graph);
-                        other_next_ipp_at
-                    },
-                    (Some(self_next_ipp_at), None) => {
+        let is_current_initially_lower = current_initial_value <= other_initial_value;
+        let mut is_current_lower_for_prev_ipp = is_current_initially_lower;
+        let mut self_next_ipp = self.next_ipp_greater_eq(0, original_graph, shortcut_graph);
+        let mut other_next_ipp = other.next_ipp_greater_eq(0, original_graph, shortcut_graph);
+        let mut better_way = vec![];
+
+        while self_next_ipp.is_some() || other_next_ipp.is_some() {
+            let ipp = match (self_next_ipp, other_next_ipp) {
+                (Some(self_next_ipp_at), Some(other_next_ipp_at)) => {
+                    if self_next_ipp_at <= other_next_ipp_at {
                         self_next_ipp = self.next_ipp_greater_eq(self_next_ipp_at + 1, original_graph, shortcut_graph);
                         self_next_ipp_at
-                    },
-                    (None, None) => panic!("while loop should have terminated")
-                };
+                    } else {
+                        other_next_ipp = other.next_ipp_greater_eq(other_next_ipp_at + 1, original_graph, shortcut_graph);
+                        other_next_ipp_at
+                    }
+                },
+                (None, Some(other_next_ipp_at)) => {
+                    other_next_ipp = other.next_ipp_greater_eq(other_next_ipp_at + 1, original_graph, shortcut_graph);
+                    other_next_ipp_at
+                },
+                (Some(self_next_ipp_at), None) => {
+                    self_next_ipp = self.next_ipp_greater_eq(self_next_ipp_at + 1, original_graph, shortcut_graph);
+                    self_next_ipp_at
+                },
+                (None, None) => panic!("while loop should have terminated")
+            };
 
-                let self_next_ipp_value = self.evaluate(ipp, original_graph, shortcut_graph);
-                let other_next_ipp_value = other.evaluate(ipp, original_graph, shortcut_graph);
+            let self_next_ipp_value = self.evaluate(ipp, original_graph, shortcut_graph);
+            let other_next_ipp_value = other.evaluate(ipp, original_graph, shortcut_graph);
 
-                let is_current_lower_for_ipp = self_next_ipp_value <= other_next_ipp_value;
-                if is_current_lower_for_ipp != is_current_lower_for_prev_ipp {
-                    better_way.push((is_current_lower_for_ipp, ipp));
-                }
-
-                is_current_lower_for_prev_ipp = is_current_lower_for_ipp;
+            let is_current_lower_for_ipp = self_next_ipp_value <= other_next_ipp_value;
+            if is_current_lower_for_ipp != is_current_lower_for_prev_ipp {
+                better_way.push((is_current_lower_for_ipp, ipp));
             }
 
-            let mut new_shortcut = Shortcut { source_data: vec![], time_data: vec![] };
+            is_current_lower_for_prev_ipp = is_current_lower_for_ipp;
+        }
 
-            let initial_better_way = better_way.last().map(|way| way.0).unwrap_or(is_current_initially_lower);
-            let mut current_better_way = initial_better_way;
-            let mut better_way_iter = better_way.iter();
-            let mut next_better_way = better_way_iter.next();
-            {
-                let mut prev_source = self.source_data.last().unwrap();
+        let mut new_shortcut = Shortcut { source_data: vec![], time_data: vec![] };
 
-                for (&time, source) in self.time_data.iter().zip(self.source_data.iter()) {
-                    if next_better_way.is_some() && time >= next_better_way.unwrap().1 {
-                        debug_assert_ne!(current_better_way, next_better_way.unwrap().0);
-                        current_better_way = next_better_way.unwrap().0;
-                        next_better_way = better_way_iter.next();
+        let initial_better_way = better_way.last().map(|way| way.0).unwrap_or(is_current_initially_lower);
+        let mut current_better_way = initial_better_way;
+        let mut better_way_iter = better_way.iter();
+        let mut next_better_way = better_way_iter.next();
+        {
+            let mut prev_source = self.source_data.last().unwrap();
 
-                        if current_better_way {
-                            new_shortcut.source_data.push(*prev_source);
-                        } else {
-                            new_shortcut.source_data.push(other.as_shortcut_data());
-                        }
-                        new_shortcut.time_data.push(next_better_way.unwrap().1);
-                    }
+            for (&time, source) in self.time_data.iter().zip(self.source_data.iter()) {
+                if next_better_way.is_some() && time >= next_better_way.unwrap().1 {
+                    debug_assert_ne!(current_better_way, next_better_way.unwrap().0);
+                    current_better_way = next_better_way.unwrap().0;
+                    next_better_way = better_way_iter.next();
 
                     if current_better_way {
-                        new_shortcut.source_data.push(*source);
-                        new_shortcut.time_data.push(time);
+                        new_shortcut.source_data.push(*prev_source);
+                    } else {
+                        new_shortcut.source_data.push(other.as_shortcut_data());
                     }
-                    prev_source = source;
+                    new_shortcut.time_data.push(next_better_way.unwrap().1);
                 }
-            }
 
-            self.source_data = new_shortcut.source_data;
-            self.time_data = new_shortcut.time_data;
+                if current_better_way {
+                    new_shortcut.source_data.push(*source);
+                    new_shortcut.time_data.push(time);
+                }
+                prev_source = source;
+            }
         }
+
+        new_shortcut
     }
 
     pub fn evaluate(&self, departure: Timestamp, original_graph: &TDGraph, shortcut_graph: &ShortcutGraph) -> Weight {
