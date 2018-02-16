2a490c9eda1e3b5cbd3a8f3a29c3739f13b0d126
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-02-16 16:07:49 +0100

    make server use CCH

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,16 +11,25 @@ extern crate bmw_routing_engine;
 
 use std::path::{Path, PathBuf};
 use std::env;
-use std::sync::RwLock;
+
+use std::thread;
+use std::sync::mpsc;
+use mpsc::{Receiver, Sender};
+use std::sync::Mutex;
+// use std::sync::RwLock;
+
 use std::cmp::Ordering;
 use rocket::response::NamedFile;
 use rocket::State;
 use rocket_contrib::Json;
 
 use bmw_routing_engine::*;
-use graph::first_out_graph::FirstOutGraph as Graph;
 use graph::*;
+use shortest_path::customizable_contraction_hierarchy;
+use shortest_path::node_order::NodeOrder;
+use shortest_path::query::customizable_contraction_hierarchy::Server;
 use io::read_into_vector;
+use bmw_routing_engine::benchmark::measure;
 
 #[derive(PartialEq,PartialOrd)]
 struct NonNan(f32);
@@ -43,26 +52,7 @@ impl Ord for NonNan {
     }
 }
 
-#[derive(Debug)]
-struct RoutingWrapper {
-    server: shortest_path::query::bidirectional_dijkstra::Server<Graph, Graph>,
-    lat: Vec<f32>,
-    lng: Vec<f32>
-}
-
-impl RoutingWrapper {
-    fn coords(&self, node: NodeId) -> (f32, f32) {
-        (self.lat[node as usize], self.lng[node as usize])
-    }
-
-    fn closest_node(&self, (p_lat, p_lng): (f32, f32)) -> NodeId {
-        self.lat.iter().zip(self.lng.iter()).enumerate().min_by_key(|&(_, (lat, lon))| {
-            NonNan::new(((lat - p_lat) * (lat - p_lat) + (lon - p_lng) * (lon - p_lng)).sqrt())
-        }).map(|(id, _)| id).unwrap() as NodeId
-    }
-}
-
-#[derive(Debug, FromForm)]
+#[derive(Debug, FromForm, Copy, Clone)]
 struct RoutingQuery {
     from_lat: f32,
     from_lng: f32,
@@ -87,39 +77,85 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 }
 
 #[get("/query?<query_params>", format = "application/json")]
-fn query(query_params: RoutingQuery, state: State<RwLock<RoutingWrapper>>) -> Json<Option<RoutingQueryResponse>> {
-    let mut wrapper = state.write().unwrap();
-    let RoutingQuery { from_lat, from_lng, to_lat, to_lng } = query_params;
-    let from = wrapper.closest_node((from_lat, from_lng));
-    let to = wrapper.closest_node((to_lat, to_lng));
+fn query(query_params: RoutingQuery, state: State<Mutex<(Sender<RoutingQuery>, Receiver<Option<RoutingQueryResponse>>)>>) -> Json<Option<RoutingQueryResponse>> {
+    let mut result = None;
 
-    match wrapper.server.distance(from, to) {
-        Some(distance) => {
-            let path = wrapper.server.path().iter().map(|&node| wrapper.coords(node)).collect();
-            Json(Some(RoutingQueryResponse { distance, path }))
-        },
-        None => Json(None)
-    }
+    measure("Total Query Request Time", || {
+        println!("Received Query: {:?}", query_params);
+
+        let (ref tx_query, ref rx_result) = *state.lock().unwrap();
+        tx_query.send(query_params).unwrap();
+        match rx_result.recv() {
+            Ok(response) => result = response,
+            Err(_) => panic!("routing engine crashed or hung up"),
+        }
+    });
+
+    println!("");
+    Json(result)
 }
 
 fn main() {
-    let mut args = env::args();
-    args.next();
+    let (tx_query, rx_query) = mpsc::channel::<RoutingQuery>();
+    let (tx_result, rx_result) = mpsc::channel::<Option<RoutingQueryResponse>>();
 
-    let arg = &args.next().expect("No directory arg given");
-    let path = Path::new(arg);
+    thread::spawn(move || {
+        let mut args = env::args();
+        args.next();
 
-    let first_out = read_into_vector(path.join("first_out").to_str().unwrap()).expect("could not read first_out");
-    let head = read_into_vector(path.join("head").to_str().unwrap()).expect("could not read head");
-    let travel_time = read_into_vector(path.join("travel_time").to_str().unwrap()).expect("could not read travel_time");
-    let graph = Graph::new(first_out, head, travel_time);
+        let arg = &args.next().expect("No directory arg given");
+        let path = Path::new(arg);
 
-    let lat = read_into_vector(path.join("latitude").to_str().unwrap()).expect("could not read first_out");
-    let lng = read_into_vector(path.join("longitude").to_str().unwrap()).expect("could not read first_out");
+        let first_out = read_into_vector(path.join("first_out").to_str().unwrap()).expect("could not read first_out");
+        let head = read_into_vector(path.join("head").to_str().unwrap()).expect("could not read head");
+        let travel_time = read_into_vector(path.join("travel_time").to_str().unwrap()).expect("could not read travel_time");
 
-    let wrapper = RoutingWrapper {
-        server: shortest_path::query::bidirectional_dijkstra::Server::<Graph, Graph>::new(graph), lat, lng
-    };
+        let lat = read_into_vector(path.join("latitude").to_str().unwrap()).expect("could not read first_out");
+        let lng = read_into_vector(path.join("longitude").to_str().unwrap()).expect("could not read first_out");
+
+        let graph = FirstOutGraph::new(first_out, head, travel_time);
+        let cch_order = read_into_vector(path.join("cch_perm").to_str().unwrap()).expect("could not read cch_perm");
+
+        let cch = customizable_contraction_hierarchy::contract(&graph, NodeOrder::from_node_order(cch_order));
+
+        let mut server = Server::new(&cch, &graph);
+
+        let coords = |node: NodeId| -> (f32, f32) {
+            (lat[node as usize], lng[node as usize])
+        };
+
+        let closest_node = |(p_lat, p_lng): (f32, f32)| -> NodeId {
+            lat.iter().zip(lng.iter()).enumerate().min_by_key(|&(_, (lat, lon))| {
+                NonNan::new(((lat - p_lat) * (lat - p_lat) + (lon - p_lng) * (lon - p_lng)).sqrt())
+            }).map(|(id, _)| id).unwrap() as NodeId
+        };
+
+        loop {
+            match rx_query.recv() {
+                Ok(query_params) => {
+                    let RoutingQuery { from_lat, from_lng, to_lat, to_lng } = query_params;
+
+                    let mut from = 0;
+                    let mut to = 0;
+                    measure("match nodes", || {
+                        from = closest_node((from_lat, from_lng));
+                        to = closest_node((to_lat, to_lng));
+                    });
+
+                    measure("cch query", || {
+                        match server.distance(from, to) {
+                            Some(distance) => {
+                                let path = server.path().iter().map(|&node| coords(node)).collect();
+                                tx_result.send(Some(RoutingQueryResponse { distance, path })).unwrap();
+                            },
+                            None => tx_result.send(None).unwrap(),
+                        }
+                    });
+                },
+                Err(_) => panic!("query connection hung up"),
+            }
+        }
+    });
 
     let config = rocket::config::Config::build(rocket::config::Environment::Staging)
         .port(8888)
@@ -127,6 +163,6 @@ fn main() {
 
     rocket::custom(config, false)
         .mount("/", routes![index, files, query])
-        .manage(RwLock::new(wrapper))
+        .manage(Mutex::new((tx_query, rx_result)))
         .launch();
 }
