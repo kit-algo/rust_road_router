cdb35e621820d95a0bb8e21e56a9333da1f27a48
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-05-14 15:10:37 +0200

    also report start of measuring

--- engine/src/benchmark/mod.rs
+++ engine/src/benchmark/mod.rs
@@ -2,7 +2,8 @@ use time;
 
 pub fn measure<Out, F: FnOnce() -> Out>(name: &str, f: F) -> Out {
     let start = time::now();
+    println!("starting {}", name);
     let res = f();
-    println!("{}: {}ms", name, (time::now() - start).num_milliseconds());
+    println!("{} done - took: {}ms", name, (time::now() - start).num_milliseconds());
     res
 }
