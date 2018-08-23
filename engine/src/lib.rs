1acfd14b55f56b3c9c19b27da91bf9c4041ff0a5
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-08-23 14:54:50 +0200

    updates for nightly

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,7 +1,5 @@
-#![feature(compiler_fences)]
 #![feature(const_fn)]
 #![feature(allocator_api)]
-#![feature(const_size_of)]
 #![feature(mpsc_select)]
 #![feature(euclidean_division)]
 #![feature(range_contains)]
