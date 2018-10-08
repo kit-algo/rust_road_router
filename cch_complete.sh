ae76ab81458c90dd488a2144bdd554294b66b586
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-10-08 11:19:49 +0200

    better cch_complete script

--- cch_complete.sh
+++ cch_complete.sh
@@ -2,13 +2,14 @@
 
 if [ ! -f /data/first_out ]; then
   echo "importing here data from /import"
-  cargo run --release -p bmw_routing_engine --bin import_here -- /import /data
+  ls -l /import
+  cargo run --release -p bmw_routing_engine --bin import_here -- /import /data || { echo 'here import failed' ; exit 1; }
   mv /data/weights /data/travel_time
 fi
 
 if [ ! -f /data/cch_perm ]; then
   echo "calculating nested dissection order with flow cutter - might take a while"
-  ./flow_cutter_cch_order.sh /data
+  ./flow_cutter_cch_order.sh /data || { echo 'calculating nested dissection order with flowcutter failed' ; exit 1; }
 fi
 
 cd server && exec cargo run --release --bin bmw_routing_server -- /data
