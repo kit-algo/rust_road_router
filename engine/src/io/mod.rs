f5533457a593fa351a5a7394a7666c6153a4a347
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2018-11-30 10:40:17 +0100

    create API for storing and reading complex objects

--- engine/src/io/mod.rs
+++ engine/src/io/mod.rs
@@ -1,9 +1,10 @@
-use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::Result;
-use std::mem;
-use std::slice;
+use std::{
+    fs::{metadata, File},
+    io::{prelude::*, Result},
+    mem,
+    slice,
+    path::Path,
+};
 
 pub trait DataBytes {
     fn data_bytes(&self) -> &[u8];
@@ -17,6 +18,12 @@ impl<T: Copy> DataBytes for [T] {
     fn data_bytes(&self) -> &[u8] {
         let num_bytes = self.len() * mem::size_of::<T>();
         unsafe { slice::from_raw_parts(self.as_ptr() as *const u8, num_bytes) }
+    }
+}
+
+impl<T: Copy> DataBytes for Vec<T> {
+    fn data_bytes(&self) -> &[u8] {
+        &self[..].data_bytes()
     }
 }
 
@@ -47,7 +54,7 @@ pub trait Load : DataBytesMut + Sized {
     fn new_with_bytes(num_bytes: usize) -> Self;
 
     fn load_from(filename: &str) -> Result<Self> {
-        let metadata = fs::metadata(filename)?;
+        let metadata = metadata(filename)?;
         let mut file = File::open(filename)?;
 
         let mut object = Self::new_with_bytes(metadata.len() as usize);
@@ -63,5 +70,46 @@ impl<T: Default + Copy> Load for Vec<T> {
         assert_eq!(num_bytes % mem::size_of::<T>(), 0);
         let num_elements = num_bytes / mem::size_of::<T>();
         (0..num_elements).map(|_| T::default()).collect()
+    }
+}
+
+pub trait Deconstruct: Sized {
+    fn store_each(&self, store_callback: &Fn(&str, &dyn Store) -> Result<()>) -> Result<()>;
+
+    fn deconstruct_to(&self, dir: &str) -> Result<()> {
+        let path = Path::new(dir);
+
+        self.store_each(&|name, object: &dyn Store| {
+            object.write_to(path.join(name).to_str().unwrap())
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Loader<'a> {
+    path: &'a Path
+}
+
+impl<'a> Loader<'a> {
+    pub fn load<T: Load>(&self, filename: &str) -> Result<T> {
+        T::load_from(self.path.join(filename).to_str().unwrap())
+    }
+}
+
+pub trait Reconstruct: Sized {
+    fn reconstruct_with(loader: Loader) -> Result<Self>;
+
+    fn reconstruct_from(dir: &str) -> Result<Self> {
+        let path = Path::new(dir);
+        Self::reconstruct_with(Loader { path })
+    }
+}
+
+pub trait ReconstructPrepared<T: Sized>: Sized {
+    fn reconstruct_with(self, loader: Loader) -> Result<T>;
+
+    fn reconstruct_from(self, dir: &str) -> Result<T> {
+        let path = Path::new(dir);
+        self.reconstruct_with(Loader { path })
     }
 }
