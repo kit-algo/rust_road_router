b6b7e486fd49367162815b8cecbc729aec9522ea
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2019-05-10 13:25:26 +0200

    clean up exp scripts a bit

--- experiments/customization_approx.rb
+++ experiments/customization_approx.rb
@@ -3,6 +3,6 @@ thresholds = [100, 500, 1000, 5000, 10000]
 
 epsilons.each do |epsilon|
   thresholds.each do |threshold|
-    `TDCCH_APPROX=#{epsilon} TDCCH_APPROX_THRESHOLD=#{threshold} cargo run --release --features 'tdcch-approx detailed-stats' --bin tdcch_customization -- /algoDaten/graphs/cleaned_td_road_data/ptv17-eur-car/day/di/ > ~/experiments/tdcch_vldb20/customization_approx/$(date --iso-8601=seconds).json`
+    `TDCCH_APPROX=#{epsilon} TDCCH_APPROX_THRESHOLD=#{threshold} cargo run --release --features 'detailed-stats' --bin tdcch_customization -- /algoDaten/graphs/cleaned_td_road_data/ptv17-eur-car/day/di/ > ~/experiments/tdcch_vldb20/customization_approx/$(date --iso-8601=seconds).json`
   end
 end
