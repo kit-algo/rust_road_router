5e1061c6854589e2430c38799211566ea495965f
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2019-05-17 15:10:14 +0200

    add binary to print queries to stdout for tds

---
+++ engine/src/bin/td_ben_queries_to_stdout.rs
@@ -0,0 +1,41 @@
+use std::{
+    env,
+    path::Path,
+    error::Error,
+};
+
+use bmw_routing_engine::{
+    io::*,
+    cli::CliErr,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = env::args();
+    args.next();
+    let arg = &args.next().ok_or_else(|| Box::new(CliErr("No directory arg given")))?;
+    let path = Path::new(arg);
+
+    let mut query_dir = None;
+    let mut base_dir = Some(path);
+
+    while let Some(base) = base_dir {
+        if base.join("source_node").exists() {
+            query_dir = Some(base);
+            break;
+        } else {
+            base_dir = base.parent();
+        }
+    }
+
+    let query_dir = query_dir.ok_or_else(|| Box::new(CliErr("No queries found")))?;
+
+    let from = Vec::<u32>::load_from(query_dir.join("source_node").to_str().unwrap())?;
+    let at = Vec::<u32>::load_from(query_dir.join("source_time").to_str().unwrap())?;
+    let to = Vec::<u32>::load_from(query_dir.join("target_node").to_str().unwrap())?;
+
+    for ((from, at), to) in from.into_iter().zip(at.into_iter()).zip(to.into_iter()) {
+        println!("{} {} {}", from, at, to);
+    }
+
+    Ok(())
+}
