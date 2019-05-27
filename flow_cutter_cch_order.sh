9745c37955abfecba8722d9b3f34cc0828011ef0
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2019-05-27 13:56:55 +0200

    report running times from flow cutter

--- flow_cutter_cch_order.sh
+++ flow_cutter_cch_order.sh
@@ -3,9 +3,11 @@
 ./lib/flow-cutter/console \
   flow_cutter_set thread_count ${2:-1} \
   load_routingkit_unweighted_graph "$1/first_out" "$1/head" \
+  report_time \
   remove_multi_arcs \
   remove_loops \
   add_back_arcs \
   sort_arcs \
   reorder_nodes_in_flow_cutter_cch_order \
+  do_not_report_time \
   save_routingkit_node_permutation_since_last_load "$1/cch_perm"
