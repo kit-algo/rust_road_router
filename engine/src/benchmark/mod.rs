fa7c73376e04551af350031eb20a8cdd54a2c278
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2019-07-24 14:54:47 +0200

    make sure nothing gets reordered boyond timing measurements

--- engine/src/benchmark/mod.rs
+++ engine/src/benchmark/mod.rs
@@ -1,11 +1,15 @@
+use std::sync::atomic::compiler_fence;
+use std::sync::atomic::Ordering::SeqCst;
 use crate::report::*;
 use time;
 
 pub fn report_time<Out, F: FnOnce() -> Out>(name: &str, f: F) -> Out {
+    compiler_fence(SeqCst);
     let start = time::now();
     eprintln!("starting {}", name);
     let res = f();
     let t_passed = (time::now() - start).num_milliseconds();
+    compiler_fence(SeqCst);
     eprintln!("{} done - took: {}ms", name, t_passed);
     report!("running_time_ms", t_passed);
     res
