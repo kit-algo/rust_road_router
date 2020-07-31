63c9c10012b0bd1d2934016e11968014ac2fa2e2
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2020-07-31 10:53:29 +0200

    improve query cmp script

--- analysis/compare_queries.py
+++ analysis/compare_queries.py
@@ -7,10 +7,12 @@ import json
 import sys
 
 baseline = json.load(open(sys.argv[1]))
-baseline_queries = pd.DataFrame.from_records([query for query in baseline['algo_runs']])
+baseline_queries = pd.DataFrame.from_records(baseline['algo_runs'])
+
+# baseline_queries['running_time_ms'] = baseline_queries['running_time_ms'] - baseline_queries['baseline_pot_init_running_time_ms']
 
 compare = json.load(open(sys.argv[2]))
-compare_queries = pd.DataFrame.from_records([query for query in compare['algo_runs']])
+compare_queries = pd.DataFrame.from_records(compare['algo_runs'])
 
 print("Baseline: {}".format(baseline['git_revision']))
 print(baseline_queries['running_time_ms'].describe())
@@ -21,3 +23,7 @@ print(compare_queries['running_time_ms'].describe())
 diff = compare_queries['running_time_ms'] - baseline_queries['running_time_ms']
 print("Difference: ")
 print(diff.describe())
+
+rel_diff = compare_queries['running_time_ms'] / baseline_queries['running_time_ms']
+print("Compare / Baseline: ")
+print(rel_diff.describe())
