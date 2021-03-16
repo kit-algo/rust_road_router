f4bdf8e7d9b82ad7d60e517cfc1fbeb2ba96d284
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2021-03-16 11:34:20 +0100

    make query comparison script more useful

--- analysis/compare_queries.py
+++ analysis/compare_queries.py
@@ -9,21 +9,26 @@ import sys
 baseline = json.load(open(sys.argv[1]))
 baseline_queries = pd.DataFrame.from_records(baseline['algo_runs'])
 
-# baseline_queries['running_time_ms'] = baseline_queries['running_time_ms'] - baseline_queries['baseline_pot_init_running_time_ms']
+# baseline_queries[key] = baseline_queries[key] - baseline_queries['baseline_pot_init_running_time_ms']
 
 compare = json.load(open(sys.argv[2]))
 compare_queries = pd.DataFrame.from_records(compare['algo_runs'])
 
+if len(sys.argv) > 3:
+  key = sys.argv[3]
+else:
+  key = 'running_time_ms'
+
 print("Baseline: {}".format(baseline['git_revision']))
-print(baseline_queries['running_time_ms'].describe())
+print(baseline_queries[key].describe())
 
 print("Compare: {}".format(compare['git_revision']))
-print(compare_queries['running_time_ms'].describe())
+print(compare_queries[key].describe())
 
-diff = compare_queries['running_time_ms'] - baseline_queries['running_time_ms']
+diff = compare_queries[key] - baseline_queries[key]
 print("Difference: ")
 print(diff.describe())
 
-rel_diff = compare_queries['running_time_ms'] / baseline_queries['running_time_ms']
+rel_diff = compare_queries[key] / baseline_queries[key]
 print("Compare / Baseline: ")
 print(rel_diff.describe())
