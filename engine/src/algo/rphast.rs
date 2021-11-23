2973c4deb327ae1f82364d3612d84a24ccafbff8
Tim "tim3z" Zeitz <dev@tim3z.net>
2021-11-23 10:52:46 +0100

    tune rphast

--- engine/src/algo/rphast.rs
+++ engine/src/algo/rphast.rs
@@ -1,13 +1,13 @@
 use super::*;
-use crate::{algo::dijkstra::*, datastr::index_heap::*, util::in_range_option::InRangeOption};
+use crate::{algo::dijkstra::*, util::in_range_option::InRangeOption};
 
 pub struct RPHAST<GF, GB> {
     order: NodeOrder,
-    restricted_distances: Vec<Weight>,
+    restricted_distances: Box<[Weight]>,
     forward: GF,
     backward: GB,
     dijkstra_data: DijkstraData<Weight>,
-    selection_queue: IndexdMinHeap<NodeIdT>,
+    selection_stack: Vec<NodeId>,
     restricted_ids: Vec<InRangeOption<NodeId>>,
     restricted_nodes: Vec<NodeId>,
     restricted_first_out: Vec<EdgeId>,
@@ -22,9 +22,9 @@ impl<GF: LinkIterGraph, GB: LinkIterGraph> RPHAST<GF, GB> {
             order,
             forward,
             backward,
-            restricted_distances: vec![INFINITY; n],
+            restricted_distances: std::iter::repeat(INFINITY).take(n).collect(),
             dijkstra_data: DijkstraData::new(n),
-            selection_queue: IndexdMinHeap::new(n),
+            selection_stack: Vec::new(),
             restricted_ids: vec![InRangeOption::NONE; n],
             restricted_nodes: Vec::new(),
             restricted_first_out: vec![0],
@@ -43,43 +43,45 @@ impl<GF: LinkIterGraph, GB: LinkIterGraph> RPHAST<GF, GB> {
         self.restricted_weight.clear();
 
         // new selection
-        debug_assert!(self.selection_queue.is_empty());
+        let mut local_idx_counter = 0;
         for &node in nodes {
             // translate node ids
             let node = self.order.rank(node);
-            self.selection_queue.push(NodeIdT(node));
-        }
+            self.selection_stack.push(node);
 
-        while let Some(NodeIdT(node)) = self.selection_queue.pop() {
-            self.restricted_nodes.push(node);
+            while let Some(&node) = self.selection_stack.last() {
+                if self.restricted_ids[node as usize].value().is_some() {
+                    self.selection_stack.pop();
+                    continue;
+                }
 
-            for link in self.backward.link_iter(node) {
-                self.selection_queue.push_unless_contained(NodeIdT(link.node));
-                self.restricted_head.push(link.node);
-                self.restricted_weight.push(link.weight);
+                let mut all_upward_done = true;
+                for link in self.backward.link_iter(node) {
+                    if self.restricted_ids[link.node as usize].value().is_none() {
+                        all_upward_done = false;
+                        self.selection_stack.push(link.node);
+                        break;
+                    }
+                }
+                if all_upward_done {
+                    self.selection_stack.pop();
+                    self.restricted_ids[node as usize] = InRangeOption::some(local_idx_counter);
+                    local_idx_counter += 1;
+
+                    for link in self.backward.link_iter(node) {
+                        self.restricted_head.push(self.restricted_ids[link.node as usize].value().unwrap());
+                        self.restricted_weight.push(link.weight);
+                    }
+                    self.restricted_first_out.push(self.restricted_head.len() as EdgeId);
+                    self.restricted_nodes.push(node);
+                }
             }
-            self.restricted_first_out.push(self.restricted_head.len() as EdgeId);
         }
-
-        // local ids, reversed
-        for (local_idx, &node) in self.restricted_nodes.iter().rev().enumerate() {
-            self.restricted_ids[node as usize] = InRangeOption::some(local_idx as NodeId);
-        }
-        for head in &mut self.restricted_head {
-            *head = self.restricted_ids[*head as usize].value().unwrap();
-        }
-        // reverse
-        self.restricted_first_out.reverse();
-        for first_out in &mut self.restricted_first_out {
-            *first_out = self.restricted_head.len() as EdgeId - *first_out;
-        }
-        self.restricted_head.reverse();
-        self.restricted_weight.reverse();
     }
 
     pub fn query(&mut self, node: NodeId) -> RPHASTResult<GF, GB> {
-        for &node in self.restricted_nodes.iter() {
-            self.restricted_distances[self.restricted_ids[node as usize].value().unwrap() as usize] = INFINITY;
+        for dist in self.restricted_distances[0..self.restricted_nodes.len()].iter_mut() {
+            *dist = INFINITY;
         }
         let mut ops = DefaultOps();
         let mut query = DijkstraRun::query(&self.forward, &mut self.dijkstra_data, &mut ops, DijkstraInit::from(self.order.rank(node)));
@@ -89,14 +91,16 @@ impl<GF: LinkIterGraph, GB: LinkIterGraph> RPHAST<GF, GB> {
             }
         }
 
-        let tails = self
-            .restricted_first_out
-            .array_windows::<2>()
-            .enumerate()
-            .flat_map(|(local_idx, &[sidx, eidx])| std::iter::repeat(local_idx as NodeId).take((eidx - sidx) as usize));
-        for ((tail, &head), &weight) in tails.zip(self.restricted_head.iter()).zip(self.restricted_weight.iter()) {
-            self.restricted_distances[tail as usize] =
-                std::cmp::min(self.restricted_distances[tail as usize], self.restricted_distances[head as usize] + weight)
+        for node in 0..self.restricted_nodes.len() {
+            unsafe {
+                let min_dist: *mut Weight = self.restricted_distances.get_unchecked_mut(node);
+                for edge_idx in *self.restricted_first_out.get_unchecked(node)..*self.restricted_first_out.get_unchecked(node + 1) {
+                    let head = *self.restricted_head.get_unchecked(edge_idx as usize);
+                    let weight = *self.restricted_weight.get_unchecked(edge_idx as usize);
+                    let head_dist = *self.restricted_distances.get_unchecked(head as usize);
+                    *min_dist = std::cmp::min(*min_dist, head_dist + weight)
+                }
+            }
         }
 
         RPHASTResult(self)
