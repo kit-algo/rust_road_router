d7dddecb842c5f33fa5dbfcbdedba7a78930d9e7
Tim "tim3z" Zeitz <dev@tim3z.net>
2022-03-24 09:41:47 +0100

    preprocessing pipeline for interval min pot

--- tdpot/src/bin/interval_min_pre.rs
+++ tdpot/src/bin/interval_min_pre.rs
@@ -1,21 +1,39 @@
 // WIP: CH potentials for TD Routing.
 
-use rust_road_router::{algo::customizable_contraction_hierarchy::*, cli::CliErr, datastr::graph::*, datastr::node_order::*, io::*};
+use rust_road_router::{
+    algo::{customizable_contraction_hierarchy::*, td_astar::IntervalMinPotential},
+    cli::CliErr,
+    datastr::graph::*,
+    datastr::node_order::*,
+    io::*,
+    report::*,
+};
 use std::{env, error::Error, path::Path};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let _reporter = enable_reporting("interval_min_pre");
     let mut args = env::args().skip(1);
     let arg = &args.next().ok_or(CliErr("No directory arg given"))?;
     let path = Path::new(arg);
+
+    let pre_out = args.next().unwrap_or("customized_corridor_mins".to_string());
+    let pot_out = args.next().unwrap_or("interval_min_pot".to_string());
 
     let graph = floating_time_dependent::TDGraph::reconstruct_from(&path)?;
     let order = NodeOrder::from_node_order(Vec::load_from(path.join("cch_perm"))?);
     let cch = CCH::fix_order_and_build(&graph, order);
 
-    let catchup = customization::ftd_for_pot::customize::<96>(&cch, &graph);
+    let catchup = report_time_with_key("preprocessing", "preprocessing", || {
+        let _blocked = block_reporting();
+        customization::ftd_for_pot::customize::<96>(&cch, &graph)
+    });
+    catchup.deconstruct_to(&path.join(pre_out))?;
 
-    let customized_folder = path.join(args.next().unwrap_or("customized_corridor_mins".to_string()));
-    catchup.deconstruct_to(&customized_folder)?;
+    let pot = report_time_with_key("build", "build", || {
+        let _blocked = block_reporting();
+        IntervalMinPotential::new(&cch, catchup)
+    });
+    pot.deconstruct_to(&path.join(pot_out))?;
 
     Ok(())
 }
--- tdpot/src/bin/interval_min_reduction.rs
+++ tdpot/src/bin/interval_min_reduction.rs
@@ -1,23 +1,39 @@
 // WIP: CH potentials for TD Routing.
 
-use rust_road_router::{algo::customizable_contraction_hierarchy::*, cli::CliErr, io::*, report::benchmark::report_time};
+use rust_road_router::{
+    algo::customizable_contraction_hierarchy::*,
+    cli::CliErr,
+    datastr::{graph::*, node_order::*},
+    io::*,
+    report::*,
+};
 use std::{env, error::Error, path::Path};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let _reporter = enable_reporting("interval_min_reduction");
+
     let mut args = env::args().skip(1);
     let arg = &args.next().ok_or(CliErr("No directory arg given"))?;
     let path = Path::new(arg);
 
-    let target_num_metrics = args.next().map(|arg| arg.parse().unwrap()).unwrap_or(24);
+    let graph = UnweightedOwnedGraph::reconstruct_from(&path)?;
+    let order = NodeOrder::from_node_order(Vec::load_from(path.join("cch_perm"))?);
+    let cch = CCH::fix_order_and_build(&graph, order);
 
-    let customized_folder = path.join(args.next().unwrap_or("customized_corridor_mins".to_string()));
-    let mut catchup = customization::ftd_for_pot::PotData::reconstruct_from(&customized_folder)?;
+    let target_num_metrics = args.next().map(|arg| arg.parse().unwrap()).unwrap_or(24);
+    let pre_in = args.next().unwrap_or("customized_corridor_mins".to_string());
+    let pre_out = args.next().unwrap_or("customized_corridor_mins".to_string());
+    let pot_out = args.next().unwrap_or("interval_min_pot".to_string());
+
+    let mut catchup = customization::ftd_for_pot::PotData::reconstruct_from(&path.join(pre_in))?;
 
     let m = catchup.fw_static_bound.len();
     let metrics: Vec<_> = catchup.fw_bucket_bounds.chunks(m).collect();
     let num_buckets = metrics.len();
     assert_eq!(catchup.bucket_to_metric.len(), num_buckets);
-    let merged = report_time("merging", || rust_road_router::algo::metric_merging::merge(&metrics, target_num_metrics));
+    let merged = report_time_with_key("merging", "build", || {
+        rust_road_router::algo::metric_merging::merge(&metrics, target_num_metrics)
+    });
     catchup.fw_bucket_bounds = merged
         .iter()
         .flat_map(|group| (0..m).map(|edge_idx| group.iter().map(|&metric_idx| metrics[metric_idx][edge_idx]).min().unwrap()))
@@ -35,8 +51,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     catchup.bucket_to_metric = bucket_to_metric;
 
-    let customized_folder = path.join(args.next().unwrap_or("customized_corridor_mins".to_string()));
-    catchup.deconstruct_to(&customized_folder)?;
+    catchup.deconstruct_to(&path.join(pre_out))?;
+
+    let pot = report_time_with_key("build", "build", || {
+        let _blocked = block_reporting();
+        rust_road_router::algo::td_astar::IntervalMinPotential::new(&cch, catchup)
+    });
+    pot.deconstruct_to(&path.join(pot_out))?;
 
     Ok(())
 }
