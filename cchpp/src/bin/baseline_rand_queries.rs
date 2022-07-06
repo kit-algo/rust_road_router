870f681f5b4188717fd7b1ca915fd43a9a5c2ce8
Tim "tim3z" Zeitz <dev@tim3z.net>
2022-07-06 10:31:31 +0200

    more baseline stats

--- cchpp/src/bin/baseline_rand_queries.rs
+++ cchpp/src/bin/baseline_rand_queries.rs
@@ -1,9 +1,13 @@
+use rand::prelude::*;
 use std::{env, error::Error, path::Path};
 
 #[macro_use]
 extern crate rust_road_router;
 use rust_road_router::{
-    algo::dijkstra::{query::dijkstra::Server, DefaultOps},
+    algo::{
+        dijkstra::{query::dijkstra::Server, DefaultOps},
+        Query, QueryServer,
+    },
     cli::CliErr,
     datastr::graph::*,
     experiments,
@@ -23,11 +27,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     let weight = Vec::<Weight>::load_from(path.join(weight_file))?;
     let topology = UnweightedOwnedGraph::reconstruct_from(&path)?;
     let graph = FirstOutGraph::new(topology.first_out(), topology.head(), &weight[..]);
+    let num_nodes = graph.num_nodes();
 
-    let mut server = Server::<_, DefaultOps, _>::new(graph.clone());
+    let mut server = Server::<_, DefaultOps, _>::new(graph);
 
-    let mut _query_ctxt = push_collection_context("queries");
-    experiments::run_random_queries(graph.num_nodes(), &mut server, &mut rng, &mut _query_ctxt, 1000);
+    let mut _queries_ctxt = push_collection_context("queries");
+    let num_queries = 1000;
+    for _ in 0..num_queries {
+        let from = rng.gen_range(0..num_nodes as NodeId);
+        let to = rng.gen_range(0..num_nodes as NodeId);
+        let _query_ctxt = _queries_ctxt.push_collection_item();
+
+        report!("from", from);
+        report!("to", to);
+
+        let mut res = silent_report_time(|| server.query_no_inline(Query { from, to }));
+        let path = silent_report_time_with_key(
+            "unpacking",
+            #[inline(never)]
+            || res.node_path(),
+        );
+        if let Some(path) = path {
+            report!("num_nodes_on_path", path.len());
+        }
+    }
 
     Ok(())
 }
