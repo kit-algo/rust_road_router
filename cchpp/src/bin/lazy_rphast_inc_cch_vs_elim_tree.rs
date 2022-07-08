95c74804bdcc6df0019b8ea5082ee93062549214
Tim "tim3z" Zeitz <dev@tim3z.net>
2022-07-08 13:22:43 +0200

    add classic chpot to cchpp lazy rphast exp

--- cchpp/src/bin/lazy_rphast_inc_cch_vs_elim_tree.rs
+++ cchpp/src/bin/lazy_rphast_inc_cch_vs_elim_tree.rs
@@ -17,6 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = Path::new(arg);
 
     let graph = WeightedGraphReconstructor("travel_time").reconstruct_from(&path)?;
+    let chpot_data = CHPotLoader::reconstruct_from(&path.join("travel_time_ch"))?;
     let cch = {
         let _blocked = block_reporting();
         let order = NodeOrder::from_node_order(Vec::load_from(path.join("cch_perm"))?);
@@ -44,6 +45,31 @@ fn main() -> Result<(), Box<dyn Error>> {
         let queries = experiments::gen_many_to_many_queries(&graph, num_queries, 2usize.pow(ball_size_exp), 2usize.pow(target_set_size_exp), &mut rng);
 
         let mut algos_ctxt = push_collection_context("algo_runs");
+        let mut many_to_one = chpot_data.potentials().0;
+        many_to_one.init(*queries.last().unwrap().1.last().unwrap());
+        many_to_one.potential(*queries.last().unwrap().0.last().unwrap());
+
+        for (sources, targets) in &queries {
+            for &target in targets.choose_multiple(&mut rng, num_queries) {
+                let _alg_ctx = algos_ctxt.push_collection_item();
+                report!("algo", "lazy_rphast_ch_many_to_one");
+                silent_report_time_with_key("selection_running_time_ms", || {
+                    many_to_one.init(target);
+                });
+                let mut progress = push_collection_context("progress_states");
+                let timer = Timer::new();
+                for (i, &s) in sources.iter().enumerate() {
+                    let i = i + 1;
+                    many_to_one.potential(s);
+                    if (i & (i - 1)) == 0 {
+                        let _state_ctx = progress.push_collection_item();
+                        report!("terminal_rank", i.trailing_zeros());
+                        report!("passed_time_mus", timer.get_passed().as_micros() as u64);
+                    }
+                }
+            }
+        }
+
         let mut many_to_one = cch_pot_data.ch_forward_potential();
         many_to_one.init(*queries.last().unwrap().1.last().unwrap());
         many_to_one.potential(*queries.last().unwrap().0.last().unwrap());
